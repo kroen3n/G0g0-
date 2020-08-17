6c4ebc60f58101cb11d2026ee6e32a56beac49a3
kroen3n <67289992+kroen3n@users.noreply.github.com>
2020-08-18 00:06:12 +0300

    Create var1.go

---
+++ unrelated/variables/var1.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main(){
+   var i string = "henlo"  //declare and initialize variable
+   j := "uarld"            // use the short variable declaration
+   fmt.Println(i, j)
+}
