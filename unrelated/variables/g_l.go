d870a70af2906f45d25ded97460f16dcb2c7945d
kroen3n <67289992+kroen3n@users.noreply.github.com>
2020-08-18 00:07:11 +0300

    Create g_l.go

---
+++ unrelated/variables/g_l.go
@@ -0,0 +1,26 @@
+package main
+
+import(
+	"fmt"
+)
+
+//global var
+var j string = "uarld"
+
+func local_var(){
+
+  //local var
+	var i string = "henlo"
+	
+	fmt.Println("I am global j called from local_var():", j)
+  fmt.Println(" ")	
+	fmt.Println("I am local i called in main() from local_var():", i)
+}
+
+func main(){
+	local_var()
+	fmt.Println(" ")
+	fmt.Println("I am global j called from main()", j)
+	//fmt.Println(" ")
+	//fmt.Println("now call local variable i: ", i)
+}
